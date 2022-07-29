68dcd64c8390d7d8fa6ede98a5e5efce93b17b79
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-07-29 17:08:09 +0100

    Create navigation bar

---
+++ portfolio/components/navbar/Navbar.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex justify-between">
+    <NavbarLogo />
+    <NavbarIcons />
+  </div>
+</template>
---
+++ portfolio/components/navbar/NavbarLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLink
+    to="/"
+    class="flex items-center space-x-3 text-gray-50 hover:text-gray-400 duration-100"
+  >
+    <img
+      class="w-8"
+      src="~/assets/images/logo.png?webp"
+      width="32"
+      height="32"
+      alt="Pink boxes logo"
+    />
+    <span class="text-2xl font-semibold">lloyd.cx</span>
+  </NuxtLink>
+</template>
