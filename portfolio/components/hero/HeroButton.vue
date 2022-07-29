bda663b4285407865a0ec519bb5cf1b323151215
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-07-29 17:39:51 +0100

    Refactor and fix hero button hyperlinks

--- portfolio/components/hero/HeroButton.vue
+++ portfolio/components/hero/HeroButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="https://github.com/LloydTao" class="flex rounded py-3 px-5">
+  <a class="flex rounded py-3 px-5">
     <div class="flex items-center text-white space-x-3">
       <slot></slot>
     </div>
--- portfolio/components/hero/HeroButtonRow.vue
+++ portfolio/components/hero/HeroButtonRow.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="flex flex-col sm:flex-row space-y-3 sm:space-x-3 sm:space-y-0">
-    <HeroButton class="bg-gray-700 hover:bg-gray-800 duration-100">
+    <HeroButton
+      href="https://github.com/LloydTao"
+      class="bg-gray-700 hover:bg-gray-800 duration-100"
+    >
       <IconWrapper>
         <GitHub />
       </IconWrapper>
       <span class="text-xl font-semibold">GitHub</span>
     </HeroButton>
-    <HeroButton class="bg-blue-500 hover:bg-blue-600 duration-100">
+    <HeroButton
+      href="https://twitter.com/LloydTao"
+      class="bg-blue-500 hover:bg-blue-600 duration-100"
+    >
       <IconWrapper>
         <Twitter />
       </IconWrapper>
