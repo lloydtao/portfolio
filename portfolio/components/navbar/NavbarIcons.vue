b231ef1ada7058695b8dd98ec735412567c1afe2
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-07-29 17:08:09 +0100

    Create navigation bar

---
+++ portfolio/components/navbar/NavbarIcons.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex space-x-5 items-center">
+    <IconLinkWrapper
+      class="text-gray-50 hover:text-gray-400 duration-100"
+      href="https://github.com/lloydtao"
+    >
+      <GitHub />
+    </IconLinkWrapper>
+    <IconLinkWrapper
+      class="text-gray-50 hover:text-gray-400 duration-100"
+      href="https://linkedin.com/in/lloydtao/"
+    >
+      <LinkedIn />
+    </IconLinkWrapper>
+    <IconLinkWrapper
+      class="text-gray-50 hover:text-gray-400 duration-100"
+      href="https://twitter.com/lloydtao"
+    >
+      <Twitter />
+    </IconLinkWrapper>
+    <IconLinkWrapper
+      class="text-gray-50 hover:text-gray-400 duration-100"
+      href="https://instagram.com/lloydtao"
+    >
+      <Instagram />
+    </IconLinkWrapper>
+  </div>
+</template>
