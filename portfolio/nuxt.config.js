e8d4624a815dce309742ada474082ae943ed6046
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-07-29 17:38:20 +0100

    Import hero components

--- portfolio/nuxt.config.js
+++ portfolio/nuxt.config.js
@@ -25,6 +25,7 @@ export default {
     dirs: [
       '~/components',
       '~/components/footer',
+      '~/components/hero',
       '~/components/icons',
       '~/components/navbar',
       '~/components/section',
