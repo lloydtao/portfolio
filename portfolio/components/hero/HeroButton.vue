bb746ff7a61c09dda9d44c930f058c755ceb71c0
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-07-29 17:38:00 +0100

    Create buttons for hero

---
+++ portfolio/components/hero/HeroButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <a href="https://github.com/LloydTao" class="flex rounded py-3 px-5">
+    <div class="flex items-center text-white space-x-3">
+      <slot></slot>
+    </div>
+  </a>
+</template>
