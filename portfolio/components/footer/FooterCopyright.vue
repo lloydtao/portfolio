a710c27569632b123e1900d3bad6aabe94309e1b
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-07-29 17:08:27 +0100

    Create footer

---
+++ portfolio/components/footer/FooterCopyright.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-gray-50 text-sm font-semibold">
+    Lewis Lloyd &copy; {{ date }}
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      date: new Date().getFullYear(),
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ portfolio/components/footer/FooterLinks.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="flex space-x-5">
+    <li>
+      <a
+        href="https://github.com/LloydTao/"
+        class="text-gray-50 hover:text-gray-400 duration-100 text-sm"
+        >GitHub</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://linkedin.com/in/LloydTao/"
+        class="text-gray-50 hover:text-gray-400 duration-100 text-sm"
+        >LinkedIn</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://twitter.com/LloydTao/"
+        class="text-gray-50 hover:text-gray-400 duration-100 text-sm"
+        >Twitter</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://www.instagram.com/lloydtao/"
+        class="text-gray-50 hover:text-gray-400 duration-100 text-sm"
+        >Instagram</a
+      >
+    </li>
+  </ul>
+</template>
