cea133d3a36e01cda586e453bebc57c326d96256
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-07-29 17:38:13 +0100

    Create hero

---
+++ portfolio/components/hero/Hero.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col lg:w-3/4 xl:w-1/2">
+    <HeroHeading />
+    <HeroButtonRow class="mt-5" />
+  </div>
+</template>
---
+++ portfolio/components/hero/HeroHeading.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p class="text-gray-300">yung software engineer &trade;</p>
+    <h1 class="text-5xl text-gray-100 uppercase font-black mt-3">
+      Lewis Lloyd
+    </h1>
+    <p class="text-xl text-gray-200 mt-5">
+      Specialising in full-stack web applications and cloud architecture, with a
+      dash of product management and technical strategy.
+    </p>
+  </div>
+</template>
