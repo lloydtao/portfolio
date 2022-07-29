2c50d0e6a8da6f1e5c18d315cf2a0a59fa7dfe78
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-07-29 17:51:37 +0100

    Add background to page

--- portfolio/layouts/default.vue
+++ portfolio/layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <div class="absolute h-full bg-gradient-to-b from-gray-800 to-gray-900">
+      <img
+        class="h-full w-auto top-0 -mt-64 md:-mt-48 lg:-mt-16 xl:mt-0"
+        src="~/assets/images/bg.svg"
+        width="1920"
+        height="1080"
+        alt="Blue and purple background graphics"
+      />
+    </div>
     <div class="absolute h-full w-full top-0">
       <div>
         <Section>
