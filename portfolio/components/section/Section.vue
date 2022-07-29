d3ff18a636cf821d09975e0efd75669276d280f8
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-07-29 17:02:21 +0100

    Create section component for page structure

---
+++ portfolio/components/section/Section.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="section">
+    <SectionContainer>
+      <slot></slot>
+    </SectionContainer>
+  </div>
+</template>
---
+++ portfolio/components/section/SectionContainer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="container" class="container mx-auto px-5 py-5">
+    <slot></slot>
+  </div>
+</template>
