d7192f29cdcb71918a63f8b020dcb0e381e14ee1
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-02 21:00:13 +0100

    Use modern ES syntax and remove comments

--- portfolio/components/graphics/GraphicsExample.vue
+++ portfolio/components/graphics/GraphicsExample.vue
@@ -17,9 +17,14 @@ export default {
       mesh: null,
     }
   },
+  mounted() {
+    this.init()
+    this.animate()
+    window.addEventListener('resize', this.resize, false)
+  },
   methods: {
-    init: function () {
-      let container = document.getElementById('canvas')
+    init() {
+      const container = document.getElementById('canvas')
 
       this.camera = new Three.PerspectiveCamera(
         70,
@@ -31,8 +36,8 @@ export default {
 
       this.scene = new Three.Scene()
 
-      let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2)
-      let material = new Three.MeshNormalMaterial()
+      const geometry = new Three.BoxGeometry(0.2, 0.2, 0.2)
+      const material = new Three.MeshNormalMaterial()
 
       this.mesh = new Three.Mesh(geometry, material)
       this.scene.add(this.mesh)
@@ -41,27 +46,22 @@ export default {
       this.renderer.setSize(container.clientWidth, container.clientHeight)
       container.appendChild(this.renderer.domElement)
     },
-    animate: function () {
+    animate() {
       requestAnimationFrame(this.animate)
       this.mesh.rotation.x += 0.01
       this.mesh.rotation.y += 0.02
       this.renderer.render(this.scene, this.camera)
     },
-    resize: function () {
-      let container = document.getElementById('canvas')
+    resize() {
+      const container = document.getElementById('canvas')
 
-      let SCREEN_WIDTH = container.clientWidth //Re-declaring variables so they are updated based on current sizes.
-      let SCREEN_HEIGHT = container.clientHeight
+      const SCREEN_WIDTH = container.clientWidth
+      const SCREEN_HEIGHT = container.clientHeight
 
-      this.camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT //Camera aspect ratio.
-      this.camera.updateProjectionMatrix() //Updating the display
-      this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT) //Setting the renderer to the height and width of the window.
+      this.camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT
+      this.camera.updateProjectionMatrix()
+      this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT)
     },
-  },
-  mounted() {
-    this.init()
-    this.animate()
-    window.addEventListener('resize', this.resize, false) //When window is resized, call onResize() function.
   },
 }
 </script>
