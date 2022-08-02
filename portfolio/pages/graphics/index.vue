9787837b13ef257e6c4190adfedd58849670bb78
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-02 18:56:47 +0100

    Create page for graphics

---
+++ portfolio/pages/graphics/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Section>
+      <h1
+        class="text-4xl sm:text-5xl text-gray-100 uppercase font-black mt-2 sm:mt-3"
+      >
+        three.js example
+      </h1>
+      <GraphicsExample />
+    </Section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'GraphicsPage',
+})
+</script>
