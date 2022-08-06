521e2a96e32ba42e437960a2974335b0387bfe2c
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-06 23:58:44 +0100

    Correct centre alignment of navbar

--- portfolio/components/navbar/Navbar.vue
+++ portfolio/components/navbar/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex justify-between">
-    <NavbarLogo />
-    <NavbarLinks />
-    <NavbarIcons />
+  <div class="flex">
+    <NavbarLogo class="justify-start flex-1" />
+    <NavbarLinks class="justify-center flex-1" />
+    <NavbarIcons class="justify-end flex-1" />
   </div>
 </template>
