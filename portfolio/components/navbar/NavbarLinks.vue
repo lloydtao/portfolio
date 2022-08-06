c43301715b1ea5924fd2181addfe4ec10467968b
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-06 16:07:49 +0100

    Add page links to navbar

---
+++ portfolio/components/navbar/NavbarLinks.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hidden sm:flex items-center space-x-5 md:space-x-8">
+    <NuxtLink
+      to="/blog"
+      class="text-gray-50 hover:text-gray-400 font-semibold duration-100"
+    >
+      Blog
+    </NuxtLink>
+    <NuxtLink
+      to="/projects"
+      class="text-gray-50 hover:text-gray-400 font-semibold duration-100"
+    >
+      Projects
+    </NuxtLink>
+    <NuxtLink
+      to="/contact"
+      class="text-gray-50 hover:text-gray-400 font-semibold duration-100"
+    >
+      Contact
+    </NuxtLink>
+  </div>
+</template>
