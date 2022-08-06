33d54efc5254ebdbb4c8e0aa4eaa7dc718dc71d9
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-06 16:44:11 +0100

    Create buttons to change cube size

--- portfolio/components/graphics/GraphicsExample.vue
+++ portfolio/components/graphics/GraphicsExample.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <div id="canvas" class="flex h-64 mt-3 sm:mt-5" />
+    <div class="flex justify-center space-x-3 mt-5">
+      <button
+        class="rounded shadow bg-gray-50 text-gray-900 font-semibold px-5 py-1"
+        v-on:click="decreaseCubeSize"
+      >
+        Smaller
+      </button>
+      <button
+        class="rounded shadow bg-gray-50 text-gray-900 font-semibold px-5 py-1"
+        v-on:click="increaseCubeSize"
+      >
+        Bigger
+      </button>
+    </div>
   </div>
 </template>
 
@@ -15,6 +29,12 @@ export default {
       scene: null,
       renderer: null,
       mesh: null,
+      cube: {
+        updated: false,
+        width: 0.1,
+        height: 0.1,
+        depth: 0.1,
+      },
     }
   },
   mounted() {
@@ -61,6 +81,32 @@ export default {
       this.camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT
       this.camera.updateProjectionMatrix()
       this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT)
+    },
+    decreaseCubeSize() {
+      if (
+        this.cube.width < 0.15 ||
+        this.cube.height < 0.15 ||
+        this.cube.depth < 0.15
+      ) {
+        return
+      }
+      this.cube.width = this.cube.width - 0.1
+      this.cube.height = this.cube.height - 0.1
+      this.cube.depth = this.cube.depth - 0.1
+      this.cube.updated = true
+    },
+    increaseCubeSize() {
+      if (
+        this.cube.width > 0.45 ||
+        this.cube.height > 0.45 ||
+        this.cube.depth > 0.45
+      ) {
+        return
+      }
+      this.cube.width = this.cube.width + 0.1
+      this.cube.height = this.cube.height + 0.1
+      this.cube.depth = this.cube.depth + 0.1
+      this.cube.updated = true
     },
   },
 }
