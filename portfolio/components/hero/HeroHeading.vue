4a65e03d71100647b4f105467471657fa43cbed4
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-07 00:00:51 +0100

    Adjust hero text and styling

--- portfolio/components/hero/HeroHeading.vue
+++ portfolio/components/hero/HeroHeading.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <p class="text-gray-300">yung software engineer &trade;</p>
-    <h1
-      class="text-4xl sm:text-5xl text-gray-100 uppercase font-black mt-2 sm:mt-3"
-    >
+  <div class="flex flex-col text-center">
     <div class="mx-auto">
       <img
         class="h-48 sm:h-64 w-48 sm:w-64 rounded-full ring-2 sm:ring-4 ring-gray-200 bg-gray-200 overflow-hidden"
         src="~/assets/images/square.png"
       />
     </div>
+    <h1 class="text-4xl text-gray-100 uppercase font-black mt-5">
       Lewis Lloyd
     </h1>
-    <p class="text-lg sm:text-xl text-gray-200 mt-3 sm:mt-5">
+    <p class="text-gray-300 mt-3">yung software engineer &trade;</p>
+    <p class="text-lg sm:text-xl text-gray-200 mt-3">
       Specialising in full-stack web applications and cloud architecture, with a
       dash of product management and technical strategy.
     </p>
