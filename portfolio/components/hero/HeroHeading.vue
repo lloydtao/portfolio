27164f23b0889dc36179eb333a0128fb3d48e213
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-07 00:00:24 +0100

    Add circle image to hero

--- portfolio/components/hero/HeroHeading.vue
+++ portfolio/components/hero/HeroHeading.vue
@@ -4,6 +4,12 @@
     <h1
       class="text-4xl sm:text-5xl text-gray-100 uppercase font-black mt-2 sm:mt-3"
     >
+    <div class="mx-auto">
+      <img
+        class="h-48 sm:h-64 w-48 sm:w-64 rounded-full ring-2 sm:ring-4 ring-gray-200 bg-gray-200 overflow-hidden"
+        src="~/assets/images/square.png"
+      />
+    </div>
       Lewis Lloyd
     </h1>
     <p class="text-lg sm:text-xl text-gray-200 mt-3 sm:mt-5">
