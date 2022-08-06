6cd78bcaa43b9f58e122ff010d2a87642322949b
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-06 16:07:37 +0100

    Create placeholder pages

---
+++ portfolio/pages/blog/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Section>
+      <h1 class="text-3xl sm:text-4xl text-gray-100 uppercase font-black">
+        Blog
+      </h1>
+      <p class="text-gray-300 mt-5">There is nothing here...</p>
+    </Section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'BlogPage',
+})
+</script>
