8b0062f8c8ce2c46bce0c9c06ab5032db78d197f
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-07 02:22:53 +0100

    Wrap hackathon art in pulsating hyperlinks

--- portfolio/components/home/HomeHackathons.vue
+++ portfolio/components/home/HomeHackathons.vue
@@ -4,36 +4,48 @@
       <h1 class="text-2xl sm:text-3xl text-gray-100 uppercase font-black">
         Hackathon Showcase
       </h1>
-
       <p class="text-xl text-gray-100 mt-3 sm:mt-5">Junction 2021</p>
       <p class="text-gray-300 mt-1">
         <span class="italic">KeepChat</span> - Chat API with adaptive hate
         speech filtering
       </p>
-      <img
-        class="h-48 sm:h-64 object-cover mt-3 rounded-lg"
-        src="~/assets/images/keepchat-architecture.jpg"
-      />
-
+      <a
+        href="https://github.com/lloydtao/Hackathon-Junction-2021"
+        class="flex items-center h-48 sm:h-64 rounded-lg hover:animate-pulse overflow-hidden mt-3"
+      >
+        <img
+          class="object-cover rounded-lg"
+          src="~/assets/images/keepchat-architecture.jpg"
+        />
+      </a>
       <p class="text-xl text-gray-100 mt-5 sm:mt-8">
         NASA Space Apps COVID-19 Challenge
       </p>
       <p class="text-gray-300 mt-1">
         <span class="italic">Bunchup</span> - Online community builder
       </p>
-      <img
-        class="h-48 sm:h-64 object-cover mt-3 rounded-lg"
-        src="~/assets/images/bunchup.jpg"
-      />
-
+      <a
+        href="https://github.com/LloydTao/Hackathon-NASA-2020"
+        class="flex items-center h-48 sm:h-64 rounded-lg hover:animate-pulse overflow-hidden mt-3"
+      >
+        <img
+          class="object-cover rounded-lg"
+          src="~/assets/images/bunchup.jpg"
+        />
+      </a>
       <p class="text-xl text-gray-100 mt-5 sm:mt-8">
         Met Office Hackathon 2020
       </p>
       <p class="text-gray-300 mt-1">Tropical cyclone visualiser</p>
-      <img
-        class="h-48 sm:h-64 object-cover mt-3 rounded-lg"
-        src="~/assets/images/tropical-cyclone.jpg"
-      />
+      <a
+        href="https://github.com/lloydtao/Hackathon-Met-Office-2020"
+        class="flex items-center h-48 sm:h-64 rounded-lg hover:animate-pulse overflow-hidden mt-3"
+      >
+        <img
+          class="object-cover rounded-lg"
+          src="~/assets/images/tropical-cyclone.jpg"
+        />
+      </a>
     </div>
   </div>
 </template>
