010478139042df17a6eb0378e8bc6dd462816769
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-07 02:07:55 +0100

    Create hackathon section

---
+++ portfolio/components/home/HomeHackathons.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex flex-col justify-end text-right items-end">
+    <div class="flex flex-col lg:w-3/4 xl:w-2/3 py-3 sm:py-8">
+      <h1 class="text-2xl sm:text-3xl text-gray-100 uppercase font-black">
+        Hackathon Showcase
+      </h1>
+
+      <p class="text-xl text-gray-100 mt-3 sm:mt-5">Junction 2021</p>
+      <p class="text-gray-300 mt-1">
+        <span class="italic">KeepChat</span> - Chat API with adaptive hate
+        speech filtering
+      </p>
+      <img
+        class="h-48 sm:h-64 object-cover mt-3 rounded-lg"
+        src="~/assets/images/keepchat-architecture.jpg"
+      />
+
+      <p class="text-xl text-gray-100 mt-5 sm:mt-8">
+        NASA Space Apps COVID-19 Challenge
+      </p>
+      <p class="text-gray-300 mt-1">
+        <span class="italic">Bunchup</span> - Online community builder
+      </p>
+      <img
+        class="h-48 sm:h-64 object-cover mt-3 rounded-lg"
+        src="~/assets/images/bunchup.jpg"
+      />
+
+      <p class="text-xl text-gray-100 mt-5 sm:mt-8">
+        Met Office Hackathon 2020
+      </p>
+      <p class="text-gray-300 mt-1">Tropical cyclone visualiser</p>
+      <img
+        class="h-48 sm:h-64 object-cover mt-3 rounded-lg"
+        src="~/assets/images/tropical-cyclone.jpg"
+      />
+    </div>
+  </div>
+</template>
