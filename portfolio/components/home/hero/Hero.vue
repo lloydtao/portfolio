410896d31f3315e9d527422a3fcab5a5a21d532a
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-09 01:46:27 +0100

    Add vertical padding to hero

--- portfolio/components/home/hero/Hero.vue
+++ portfolio/components/home/hero/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col lg:w-3/4 xl:w-2/3 pb-3 sm:pb-8 mx-auto">
+  <div class="flex flex-col lg:w-3/4 xl:w-2/3 sm:pt-3 pb-3 sm:pb-8 mx-auto">
     <HeroHeading />
     <HeroButtonRow class="mt-5" />
   </div>
