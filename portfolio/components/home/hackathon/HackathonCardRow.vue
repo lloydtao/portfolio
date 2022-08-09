4f4464285e83aad29bceda4a6984cb12c9f99738
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-09 17:25:10 +0100

    Replace hackathon jpegs with pngs

--- portfolio/components/home/hackathon/HackathonCardRow.vue
+++ portfolio/components/home/hackathon/HackathonCardRow.vue
@@ -4,7 +4,7 @@
       title="Junction 2021"
       project="KeepChat"
       description="Programmable chat API with adaptive hate speech filtering, designed to be integrated into video games as a service"
-      image="/images/keepchat-architecture.jpg"
+      image="/images/keepchat.png"
       date="19th November 2021"
       link="https://dev.to/tao/keepchat-managed-chat-api-with-adaptive-hate-speech-filtering-2o0l"
     />
@@ -12,7 +12,7 @@
       title="NASA Space Apps COVID-19 Challenge"
       project="Bunchup"
       description="Online community builder and event scheduler to tackle loneliness, especially during COVID-19"
-      image="/images/bunchup.jpg"
+      image="/images/bunchup.png"
       date="30th May 2020"
       link="https://covid19.spaceappschallenge.org/challenges/covid-challenges/isolation-solution/teams/code-scamps/project"
     />
@@ -20,7 +20,7 @@
       title="Met Office Hackathon 2020"
       project="Tropical cyclone visualiser"
       description="Visualise the path, speed and intensity of historic tropical cyclones in 3D, and filter via web interface"
-      image="/images/tropical-cyclone.jpg"
+      image="/images/cyclones.png"
       date="25th January 2020"
       link="https://github.com/LloydTao/Hackathon-Met-Office-2020"
     >
