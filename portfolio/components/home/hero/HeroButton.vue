cba43d7612edf5e6e16c42e52fb4f68bf24cdee4
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-09 01:46:35 +0100

    Add shadows to hero

--- portfolio/components/home/hero/HeroButton.vue
+++ portfolio/components/home/hero/HeroButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a class="flex rounded py-3 px-5">
+  <a class="flex rounded shadow py-3 px-5">
     <div class="flex items-center text-white space-x-3">
       <slot></slot>
     </div>
