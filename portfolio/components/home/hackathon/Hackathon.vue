33f654d66fbf2eeb37fcbd414a3ff06a08f2e229
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-09 02:00:04 +0100

    Add subheading to hackathon section

--- portfolio/components/home/hackathon/Hackathon.vue
+++ portfolio/components/home/hackathon/Hackathon.vue
@@ -3,6 +3,9 @@
     <h1 class="text-3xl text-gray-100 uppercase font-black">
       Hackathon showcase
     </h1>
+    <h1 class="text-lg text-gray-200 mt-2">
+      Applications built in less than a weekend
+    </h1>
     <HackathonCardRow class="mt-5" />
   </div>
 </template>
