bb3e37b346f163082c6ed2f8a8d4a8f78913525e
lloydtao <41923457+lloydtao@users.noreply.github.com>
2022-08-09 16:39:11 +0000

    Deploying to gh-pages from @ lloydtao/portfolio@cbcc86ca5dc910acb301abe7aa9e0b08608b84df 🚀

---
+++ _nuxt/8be1efe.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{346:function(t,n,l){"use strict";l.r(n);var e=l(3),component=Object(e.a)({},(function(){return(0,this._self._c)("div",{staticClass:"h-6 w-6"},[this._t("default")],2)}),[],!1,null,null,null);n.default=component.exports}}]);
