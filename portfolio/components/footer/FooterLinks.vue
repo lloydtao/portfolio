efcef094386b1b6caa6e704fb707f01654520bc2
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-13 00:55:02 +0100

    Remove social links from footer

--- portfolio/components/footer/FooterLinks.vue
+++ portfolio/components/footer/FooterLinks.vue
@@ -57,38 +57,5 @@
         </NuxtLink>
       </li>
     </ul>
-    <ul class="flex flex-col">
-      <li>
-        <p class="text-gray-100 font-semibold text-sm">Social</p>
-      </li>
-      <li>
-        <a
-          href="https://github.com/LloydTao/"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
-          >GitHub</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://linkedin.com/in/LloydTao/"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
-          >LinkedIn</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/LloydTao/"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://www.instagram.com/lloydtao/"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
-          >Instagram</a
-        >
-      </li>
-    </ul>
   </div>
 </template>
