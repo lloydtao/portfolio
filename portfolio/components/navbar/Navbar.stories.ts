008943e4baf518146723963561070755e88525a6
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-13 14:50:17 +0100

    Add navbar to storybook

---
+++ portfolio/components/navbar/Navbar.stories.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'Navbar',
+}
+
+export const Navbar = () => '<Navbar />'
+export const NavbarIcons = () => '<NavbarIcons />'
+export const NavbarLinks = () => '<NavbarLinks />'
+export const NavbarLogo = () => '<NavbarLogo />'
