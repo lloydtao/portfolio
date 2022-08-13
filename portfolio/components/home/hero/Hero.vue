d0a5504d843e225876818455728804f474bce667
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-13 16:47:15 +0100

    Refactor alignment of hero buttons

--- portfolio/components/home/hero/Hero.vue
+++ portfolio/components/home/hero/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col lg:w-3/4 xl:w-2/3 sm:pt-3 pb-3 sm:pb-8 mx-auto">
     <HeroHeading />
-    <HeroButtonRow class="mt-5" />
+    <HeroButtonRow class="flex justify-center mt-5" />
   </div>
 </template>
