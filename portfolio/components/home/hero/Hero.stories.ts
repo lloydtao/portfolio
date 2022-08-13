7fcfe88e8335d73e205a0c2e1a5ae742c6d57f60
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-13 15:56:12 +0100

    Add hero to storybook

---
+++ portfolio/components/home/hero/Hero.stories.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'Hero',
+}
+
+export const Hero = () => '<Hero />'
+export const HeroButtons = () => '<HeroButtonRow />'
+export const HeroHeading = () => '<HeroHeading />'
