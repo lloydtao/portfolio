5b51bd0d3e3de056810cd1d8b653995614c888d4
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-13 14:49:13 +0100

    Add footer to storybook

---
+++ portfolio/components/footer/Footer.stories.ts
@@ -0,0 +1,7 @@
+export default {
+  title: 'Footer',
+}
+
+export const Footer = () => '<Footer />'
+export const FooterCopyright = () => '<FooterCopyright />'
+export const FooterLinks = () => '<FooterLinks />'
