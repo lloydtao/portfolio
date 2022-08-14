90a759a590fa46faca8a6531cc5e72aecd85331c
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-14 14:54:15 +0100

    Add storybook iframe to site

--- portfolio/components/storybook/Storybook.vue
+++ portfolio/components/storybook/Storybook.vue
@@ -8,6 +8,33 @@
         Interactive showcase of components used on this site.
       </p>
     </div>
+    <div class="flex flex-col rounded-lg overflow-hidden mt-5">
+      <a
+        class="flex items-center space-x-2 text-gray-50 bg-gray-600 hover:bg-gray-500 shadow font-semibold duration-100 px-5 py-2 justify-end"
+        :href="storybookUrl"
+      >
+        <p>Full screen</p>
+        <IconWrapper>
+          <Expand />
+        </IconWrapper>
+      </a>
+      <iframe
+        title="Storybook | UI Documentation"
+        class="flex w-full"
+        height="640"
+        :src="storybookUrl"
+        frameborder="0"
+        scrolling="no"
+      ></iframe>
+    </div>
+    <p class="text-gray-300 mt-5">
+      Powered by
+      <a
+        class="text-gray-50 hover:text-gray-400 font-semibold duration-100"
+        href="https://storybook.js.org/"
+        >Storybook</a
+      >, a great tool for UI documentation.
+    </p>
   </div>
 </template>
 
