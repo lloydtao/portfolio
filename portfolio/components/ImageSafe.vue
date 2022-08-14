af4c8ddc9c36f33996df646ee00bf535ef9b715f
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-14 01:53:37 +0100

    Use nuxt-img only if server-side rendering

---
+++ portfolio/components/ImageSafe.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <nuxt-img
+      v-if="server"
+      :class="class_"
+      :src="src"
+      :width="width"
+      :height="height"
+      :alt="alt"
+    />
+    <img
+      v-else
+      :class="class_"
+      :src="src"
+      :width="width"
+      :height="height"
+      :alt="alt"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      server: process.server,
+    }
+  },
+  props: {
+    class_: { type: String, default: '' },
+    src: { type: String, default: '' },
+    width: { type: String, default: '' },
+    height: { type: String, default: '' },
+    alt: { type: String, default: '' },
+  },
+}
+</script>
