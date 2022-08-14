d977b1bae8ff8b5a65b39147e0e1b12ad96a1c6e
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-14 14:53:53 +0100

    Create storybook landing page

---
+++ portfolio/components/storybook/Storybook.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="text-3xl sm:text-4xl text-gray-100 uppercase font-black">
+      Component library
+    </h1>
+    <div class="flex flex-col sm:flex-row sm:justify-between">
+      <p class="text-gray-300 mt-3">
+        Interactive showcase of components used on this site.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storybookUrl:
+        process.env.storybookBaseUrl + '?path=/story/section--section-demo',
+    }
+  },
+}
+</script>
---
+++ portfolio/pages/storybook/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Section>
+      <Storybook />
+    </Section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'StorybookPage',
+})
+</script>
