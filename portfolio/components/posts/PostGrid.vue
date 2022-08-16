b6a9ec02fac871cd80d89f15f0a80d8df8d00985
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-16 16:12:23 +0100

    Create post grid components

---
+++ portfolio/components/posts/PostGrid.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3"
+  >
+    <PostCard
+      v-for="post in posts"
+      v-show="post.published"
+      :key="post.slug"
+      :route="route"
+      :slug="post.slug"
+      :title="post.title"
+      :subtitle="post.subtitle"
+      :series="post.series"
+      :description="post.description"
+      :published-at="post.publishedAt"
+    />
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    route: { type: String, default: '' },
+    posts: {
+      type: Array,
+      default() {
+        return []
+      },
+    },
+  },
+}
+</script>
