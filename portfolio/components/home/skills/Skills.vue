7990c70f1d5e7e09e829d3119dbac2040dd60972
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-16 16:31:24 +0100

    Refresh skills grid

--- portfolio/components/home/skills/Skills.vue
+++ portfolio/components/home/skills/Skills.vue
@@ -1,73 +1,61 @@
 <template>
   <div>
     <SkillsHeading />
-    <div class="flex flex-col space-y-3 mt-5">
-      <div
-        class="flex space-x-0 sm:space-x-3 space-y-3 sm:space-y-0 flex-col sm:flex-row"
+    <div class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-3 mt-3">
+      <SkillsCard
+        class="flex-1"
+        skill="Frontend"
+        description="JavaScript, Vue, Tailwind"
       >
-        <SkillsCard
-          class="flex-1"
-          skill="Frontend"
-          description="JavaScript, Vue, Tailwind"
-        >
-          <template v-slot:icon>
-            <IconWrapper class="text-gray-50">
-              <LinkedIn />
-            </IconWrapper>
-          </template>
-        </SkillsCard>
-        <SkillsCard class="flex-1" skill="Backend" description="Django, PHP">
-          <template v-slot:icon>
-            <IconWrapper class="text-gray-50">
-              <LinkedIn />
-            </IconWrapper>
-          </template>
-        </SkillsCard>
-        <SkillsCard
-          class="flex-1"
-          skill="DevOps"
-          description="AWS, Git, Docker"
-        >
-          <template v-slot:icon>
-            <IconWrapper class="text-gray-50">
-              <LinkedIn />
-            </IconWrapper>
-          </template>
-        </SkillsCard>
-      </div>
-      <div
-        class="flex space-x-0 sm:space-x-3 space-y-3 sm:space-y-0 flex-col sm:flex-row"
+        <template v-slot:icon>
+          <IconWrapperSolid class="text-gray-50">
+            <Template />
+          </IconWrapperSolid>
+        </template>
+      </SkillsCard>
+      <SkillsCard class="flex-1" skill="Backend" description="Django, PHP">
+        <template v-slot:icon>
+          <IconWrapperSolid class="text-gray-50">
+            <Server />
+          </IconWrapperSolid>
+        </template>
+      </SkillsCard>
+      <SkillsCard class="flex-1" skill="DevOps" description="AWS, Git, Docker">
+        <template v-slot:icon>
+          <IconWrapperSolid class="text-gray-50">
+            <Cloud />
+          </IconWrapperSolid>
+        </template>
+      </SkillsCard>
+      <SkillsCard class="flex-1" skill="Database" description="SQL, Postgres">
+        <template v-slot:icon>
+          <IconWrapperSolid class="text-gray-50">
+            <Database />
+          </IconWrapperSolid>
+        </template>
+      </SkillsCard>
+      <SkillsCard
+        class="flex-1"
+        skill="API Design"
+        description="REST, JWT, Serverless"
       >
-        <SkillsCard class="flex-1" skill="Database" description="SQL, Postgres">
-          <template v-slot:icon>
-            <IconWrapper class="text-gray-50">
-              <LinkedIn />
-            </IconWrapper>
-          </template>
-        </SkillsCard>
-        <SkillsCard
-          class="flex-1"
-          skill="API Design"
-          description="REST, JWT, Serverless"
-        >
-          <template v-slot:icon>
-            <IconWrapper class="text-gray-50">
-              <LinkedIn />
-            </IconWrapper>
-          </template>
-        </SkillsCard>
-        <SkillsCard
-          class="flex-1"
-          skill="Machine Learning"
-          description="Python, scikit-learn"
-        >
-          <template v-slot:icon>
-            <IconWrapper class="text-gray-50">
-              <LinkedIn />
-            </IconWrapper>
-          </template>
-        </SkillsCard>
-      </div>
+        <template v-slot:icon>
+          <IconWrapperSolid class="text-gray-50">
+            <Link />
+          </IconWrapperSolid>
+        </template>
+      </SkillsCard>
+      <SkillsCard
+        class="flex-1"
+        skill="Machine Learning"
+        description="Python, scikit-learn"
+      >
+        <template v-slot:icon>
+          <IconWrapperSolid class="text-gray-50">
+            <Chip />
+          </IconWrapperSolid>
+        </template>
+      </SkillsCard>
     </div>
   </div>
 </template>
