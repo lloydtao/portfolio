f3d4ddada1aca98f7ebbcaa739891a3a6ed802fa
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-08-16 16:13:42 +0100

    Add post grid to projects page

--- portfolio/pages/projects/index.vue
+++ portfolio/pages/projects/index.vue
@@ -4,7 +4,14 @@
       <h1 class="text-3xl sm:text-4xl text-gray-100 uppercase font-black">
         Projects
       </h1>
-      <p class="text-gray-300 mt-5">There is nothing here...</p>
+      <p class="text-lg text-gray-200 mt-2">How I've kept myself busy</p>
+
+      <h2 class="text-2xl sm:text-3xl text-gray-200 uppercase font-black mt-5">
+        Hackathons
+      </h2>
+      <div class="mt-5">
+        <PostGrid :posts="hackathons" route="projects-slug" />
+      </div>
     </Section>
   </div>
 </template>
@@ -14,5 +21,23 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'ProjectPage',
+  async asyncData({ $content, params }) {
+    const hackathons = await $content('projects')
+      .only([
+        'slug',
+        'title',
+        'subtitle',
+        'description',
+        'series',
+        'publishedAt',
+        'published',
+      ])
+      .where({ series: 'Hackathons' })
+      .sortBy('publishedAt', 'desc')
+      .fetch()
+    return {
+      hackathons,
+    }
+  },
 })
 </script>
