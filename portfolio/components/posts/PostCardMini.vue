65061c632807bb006ddf38ce1b492a00a3847703
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-10-23 21:43:07 +0100

    Create leetcode post directory

---
+++ portfolio/components/posts/PostCardMini.vue
@@ -0,0 +1,32 @@
+<template>
+  <li class="w-full">
+    <NuxtLink :to="{ name: route, params: { slug: slug } }">
+      <div
+        class="h-full bg-gray-800 hover:bg-gray-700 border border-gray-600 hover:border-gray-500 py-3 duration-100"
+      >
+        <h1 class="font-semibold text-white px-5">{{ title }}</h1>
+        <h2 class="text-sm text-gray-300 px-5">
+          {{ formatDate(publishedAt) }} · {{ series }}
+        </h2>
+      </div>
+    </NuxtLink>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    route: { type: String, default: 'page-slug' },
+    slug: { type: String, default: 'slug' },
+    title: { type: String, default: 'Title' },
+    series: { type: String, default: 'Series' },
+    publishedAt: { type: String, default: '2000-01-01T00:00:00.000Z' },
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    },
+  },
+}
+</script>
---
+++ portfolio/pages/leetcode/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Section>
+      <h1 class="text-3xl sm:text-4xl text-gray-100 uppercase font-black">
+        Leetcode
+      </h1>
+      <h2 class="text-2xl sm:text-3xl text-gray-200 uppercase font-black mt-5">
+        Solutions
+      </h2>
+      <div class="mt-5">
+        <PostGridMini :posts="leetcode" route="leetcode-slug" />
+      </div>
+    </Section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LeetcodePage',
+  async asyncData({ $content }) {
+    const leetcode = await $content('leetcode')
+      .only([
+        'slug',
+        'title',
+        'series',
+        'publishedAt',
+        'published',
+      ])
+      .sortBy('publishedAt', 'desc')
+      .fetch()
+    return {
+      leetcode,
+    }
+  },
+})
+</script>
