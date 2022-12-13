95aa4fcc4b8aa27e799d299514df660f8ee596f4
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-13 22:08:48 +0000

    Add mobile navbar

--- portfolio/components/navbar/Navbar.vue
+++ portfolio/components/navbar/Navbar.vue
@@ -3,5 +3,6 @@
     <NavbarLogo class="justify-start flex-1" />
     <NavbarLinks class="justify-center flex-1" />
     <NavbarIcons class="justify-end flex-1" />
+    <NavbarDropdown class="justify-end flex-1" />
   </div>
 </template>
