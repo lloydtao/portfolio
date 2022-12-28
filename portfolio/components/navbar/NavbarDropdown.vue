8bb0ae0c244b1838a124f63dd7e496dfc01ca9d5
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-28 15:50:45 +0000

    Add blog link to navbar and footer

--- portfolio/components/navbar/NavbarDropdown.vue
+++ portfolio/components/navbar/NavbarDropdown.vue
@@ -3,17 +3,17 @@
     <li>
       <a
         href="#"
-        class="flex-none absolute right-0 px-5 z-30"
+        class="absolute right-0 z-30 flex-none px-5"
         @click="showMenu = !showMenu"
         ><p
-          class="text-gray-200 font-semibold tracking-wide hover:bg-gray-900 hover:bg-opacity-30 rounded px-2 py-2"
+          class="px-2 py-2 font-semibold tracking-wide text-gray-200 rounded hover:bg-gray-900 hover:bg-opacity-30"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
-            class="h-6 w-6"
+            class="w-6 h-6"
           >
             <path
               stroke-linecap="round"
@@ -24,7 +24,7 @@
           </svg></p
       ></a>
       <div
-        class="fixed inset-0 z-10 bg-black transition"
+        class="fixed inset-0 z-10 transition bg-black"
         :class="{
           'invisible opacity-0': !showMenu,
           'visible opacity-40': showMenu,
@@ -41,31 +41,38 @@
         }"
       >
         <ul
-          class="flex flex-col bg-gray-50 w-full rounded shadow-md overflow-hidden"
+          class="flex flex-col w-full overflow-hidden rounded shadow-md bg-gray-50"
         >
           <li class="flex">
-            <h3 class="flex flex-1 text-gray-800 font-semibold px-5 py-4">
+            <h3 class="flex flex-1 px-5 py-4 font-semibold text-gray-800">
               Navigation
             </h3>
           </li>
           <li class="flex border-t" @click="showMenu = !showMenu">
             <NuxtLink
               :to="`/`"
-              class="flex flex-1 text-lg text-gray-700 hover:bg-gray-100 font-semibold px-8 py-4"
+              class="flex flex-1 px-8 py-4 text-lg font-semibold text-gray-700 hover:bg-gray-100"
               >Home</NuxtLink
             >
           </li>
           <li class="flex" @click="showMenu = !showMenu">
             <NuxtLink
+              :to="`/blog/`"
+              class="flex flex-1 px-8 py-4 text-lg font-semibold text-gray-700 hover:bg-gray-100"
+              >Blog</NuxtLink
+            >
+          </li>
+          <li class="flex" @click="showMenu = !showMenu">
+            <NuxtLink
               :to="`/projects/`"
-              class="flex flex-1 text-lg text-gray-700 hover:bg-gray-100 font-semibold px-8 py-4"
+              class="flex flex-1 px-8 py-4 text-lg font-semibold text-gray-700 hover:bg-gray-100"
               >Projects</NuxtLink
             >
           </li>
           <li class="flex" @click="showMenu = !showMenu">
             <NuxtLink
               :to="`/contact/`"
-              class="flex flex-1 text-lg text-gray-700 hover:bg-gray-100 font-semibold px-8 py-4"
+              class="flex flex-1 px-8 py-4 text-lg font-semibold text-gray-700 hover:bg-gray-100"
               >Contact</NuxtLink
             >
           </li>
