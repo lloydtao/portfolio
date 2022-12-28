c5134e2fff3e3974734e332f898d9edbbf2a086a
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-28 15:50:45 +0000

    Add blog link to navbar and footer

--- portfolio/components/footer/FooterLinks.vue
+++ portfolio/components/footer/FooterLinks.vue
@@ -2,12 +2,20 @@
   <div class="flex space-x-12">
     <ul class="flex flex-col">
       <li>
-        <p class="text-gray-100 font-semibold text-sm">Pages</p>
+        <p class="text-sm font-semibold text-gray-100">Pages</p>
+      </li>
+      <li>
+        <NuxtLink
+          to="/blog/"
+          class="text-sm text-gray-300 duration-100 hover:text-gray-400"
+        >
+          Blog
+        </NuxtLink>
       </li>
       <li>
         <NuxtLink
           to="/projects/"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
+          class="text-sm text-gray-300 duration-100 hover:text-gray-400"
         >
           Projects
         </NuxtLink>
@@ -15,7 +23,7 @@
       <li>
         <NuxtLink
           to="/contact/"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
+          class="text-sm text-gray-300 duration-100 hover:text-gray-400"
         >
           Contact
         </NuxtLink>
@@ -23,19 +31,19 @@
     </ul>
     <ul class="flex flex-col">
       <li>
-        <p class="text-gray-100 font-semibold text-sm">Resources</p>
+        <p class="text-sm font-semibold text-gray-100">Resources</p>
       </li>
       <li>
         <a
           href="https://github.com/lloydtao/portfolio"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
+          class="text-sm text-gray-300 duration-100 hover:text-gray-400"
           >Source code</a
         >
       </li>
       <li>
         <NuxtLink
           to="/storybook/"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
+          class="text-sm text-gray-300 duration-100 hover:text-gray-400"
         >
           Storybook
         </NuxtLink>
@@ -43,12 +51,12 @@
     </ul>
     <ul class="flex flex-col">
       <li>
-        <p class="text-gray-100 font-semibold text-sm">Other</p>
+        <p class="text-sm font-semibold text-gray-100">Other</p>
       </li>
       <li>
         <NuxtLink
           to="/leetcode/"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
+          class="text-sm text-gray-300 duration-100 hover:text-gray-400"
         >
           Leetcode
         </NuxtLink>
@@ -56,7 +64,7 @@
       <li>
         <NuxtLink
           to="/graphics/"
-          class="text-gray-300 hover:text-gray-400 duration-100 text-sm"
+          class="text-sm text-gray-300 duration-100 hover:text-gray-400"
         >
           Graphics
         </NuxtLink>
--- portfolio/components/navbar/NavbarLinks.vue
+++ portfolio/components/navbar/NavbarLinks.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="hidden sm:flex items-center space-x-5 md:space-x-8">
+  <div class="items-center hidden space-x-5 sm:flex md:space-x-8">
+    <NuxtLink
+      to="/blog/"
+      class="font-semibold duration-100 text-gray-50 hover:text-gray-400"
+    >
+      Blog
+    </NuxtLink>
     <NuxtLink
       to="/projects/"
-      class="text-gray-50 hover:text-gray-400 font-semibold duration-100"
+      class="font-semibold duration-100 text-gray-50 hover:text-gray-400"
     >
       Projects
     </NuxtLink>
     <NuxtLink
       to="/contact/"
-      class="text-gray-50 hover:text-gray-400 font-semibold duration-100"
+      class="font-semibold duration-100 text-gray-50 hover:text-gray-400"
     >
       Contact
     </NuxtLink>
