52aaa1dec984f13e924e8da41f62ef8d135cdb69
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-30 13:52:11 +0000

    Add Nuxt posts to blog

--- portfolio/pages/blog/index.vue
+++ portfolio/pages/blog/index.vue
@@ -4,9 +4,23 @@
       <h1 class="text-3xl sm:text-4xl font-black text-gray-100 uppercase">
         Blog
       </h1>
-      <p class="mt-3 text-lg text-gray-300">How I've kept myself busy</p>
+      <h2 class="mt-5 text-2xl sm:text-3xl font-black text-gray-200 uppercase">
+        General
+      </h2>
+      <p class="mt-3 text-lg text-gray-300">
+        Writings on software engineering and technology
+      </p>
       <div class="mt-5">
         <PostGrid :posts="blog" route="blog-slug" />
+      </div>
+    </Section>
+    <Section>
+      <h2 class="mt-5 text-2xl sm:text-3xl font-black text-gray-200 uppercase">
+        Nuxt
+      </h2>
+      <p class="mt-3 text-lg text-gray-300">The Intuitive Web Framework</p>
+      <div class="mt-5">
+        <PostGrid :posts="articlesNuxt" route="blog-slug" />
       </div>
     </Section>
   </div>
@@ -31,7 +45,21 @@ export default Vue.extend({
       ])
       .sortBy('publishedAt', 'desc')
       .fetch()
+    const articlesNuxt = await $content('blog/nuxt')
+      .only([
+        'slug',
+        'title',
+        'subtitle',
+        'description',
+        'series',
+        'localimage',
+        'publishedAt',
+        'published',
+      ])
+      .sortBy('publishedAt', 'desc')
+      .fetch()
     return {
+      articlesNuxt,
       blog,
     }
   },
