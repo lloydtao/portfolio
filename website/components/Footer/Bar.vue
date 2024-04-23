89a67aac4af61bb3ab51cd8c85c09e485445b1ee
Lewis Lloyd <41923457+lloydtao@users.noreply.github.com>
2024-04-23 23:29:09 +0100

    Add footer to layout

---
+++ website/components/Footer/Bar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="flex flex-col sm:flex-row items-center sm:items-start space-y-5 sm:space-y-0"
+  >
+    <FooterCopyright class="flex-1 justify-start" />
+    <FooterLinks class="flex-1 justify-end" />
+  </div>
+</template>
---
+++ website/components/Footer/Links.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="flex space-x-12">
+    <ul class="flex flex-col">
+      <li>
+        <p class="text-sm font-semibold text-gray-100">Pages</p>
+      </li>
+      <li>
+        <NuxtLink
+          to="/"
+          class="text-sm text-gray-300 hover:text-gray-400 duration-100"
+        >
+          Home
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink
+          to="/blog"
+          class="text-sm text-gray-300 hover:text-gray-400 duration-100"
+        >
+          Blog
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink
+          to="/projects"
+          class="text-sm text-gray-300 hover:text-gray-400 duration-100"
+        >
+          Projects
+        </NuxtLink>
+      </li>
+      <li>
+        <NuxtLink
+          to="/contact"
+          class="text-sm text-gray-300 hover:text-gray-400 duration-100"
+        >
+          Contact
+        </NuxtLink>
+      </li>
+    </ul>
+    <ul class="flex flex-col">
+      <li>
+        <p class="text-sm font-semibold text-gray-100">Resources</p>
+      </li>
+      <li>
+        <a
+          href="https://github.com/lloydtao/portfolio"
+          class="text-sm text-gray-300 hover:text-gray-400 duration-100"
+          >Source code</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
