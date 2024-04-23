ccbe5406de9b3d1b660e3386da18682f00c26c87
Lewis Lloyd <41923457+lloydtao@users.noreply.github.com>
2024-04-23 23:58:19 +0100

    Use client DB for Nuxt Content to fix 404

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
+  content: {
+    experimental: {
+      clientDB: true,
+    },
+  },
   modules: [
     "@nuxt/eslint",
     "@nuxtjs/tailwindcss",
